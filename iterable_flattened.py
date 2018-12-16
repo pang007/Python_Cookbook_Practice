24417a46a053353ef1f9bad0cbd924699b5cf5d6
pang007 <pangkachun@gmail.com>
2018-12-16 15:38:25 +0800

    Add files via upload

---
+++ iterable_flattened.py
@@ -0,0 +1,20 @@
+"""
+Input: a nested list consisting of integers  -> list
+Output: a flattened list consisting the element inside
+"""
+
+from collections import Iterable
+
+nested_list = [1,[2,3,4],[5,6,7],[8,9]]
+
+def flatten(nested_list, ignore_type = (str)):
+	for item in nested_list:
+		# str is an iterable so we have to check the item is not a str
+		if isinstance(item, Iterable) and not isinstance(item, ignore_type):
+			# as flatten is another iterable, hence we have to use yield from
+			yield from flatten(item)
+		else:
+			yield item
+
+for x in flatten(nested_list):
+	print(x)
